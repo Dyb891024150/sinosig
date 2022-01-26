34b72342684a64f5365974543fd1647c20c1a982
“e <dingyanbiao-ghq@sinosig.com>
2022-01-26 18:01:20 +0800

    描述

---
+++ asset-admin/src/main/java/com/sinosig/web/controller/system/SysIndexController.java
@@ -0,0 +1,29 @@
+package com.sinosig.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.sinosig.common.config.RuoYiConfig;
+import com.sinosig.common.utils.StringUtils;
+
+/**
+ * 首页
+ *
+ * @author ruoyi
+ */
+@RestController
+public class SysIndexController
+{
+    /** 系统基础配置 */
+    @Autowired
+    private RuoYiConfig ruoyiConfig;
+
+    /**
+     * 访问首页，提示语
+     */
+    @RequestMapping("/")
+    public String index()
+    {
+        return "ok";
+    }
+}
---
+++ asset-admin/src/main/java/com/sinosig/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.sinosig.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.sinosig.common.core.controller.BaseController;
+import com.sinosig.common.core.domain.AjaxResult;
+import com.sinosig.common.core.domain.model.RegisterBody;
+import com.sinosig.common.utils.StringUtils;
+import com.sinosig.framework.web.service.SysRegisterService;
+import com.sinosig.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
